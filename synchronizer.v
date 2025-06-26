cd0517c33a7a9cdf80f9707ae5a6519a8d1013a1
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 02:08:53 +0530

    Update synchronizer.v

--- synchronizer.v
+++ synchronizer.v
@@ -1,1 +1,16 @@
+module twoflopsync #(parameter addr_size=4) (input clk,rstn,input [addr_size:0] ptr,output reg [addr_size:0] q2);
+  reg [add_size:0] q1;
 
+  always@(posedge clk or negedge rstn) begin
+    
+    if(!rstn) begin
+      q2<=0;
+      q1<=0;end
+    else begin
+      q1<=ptr;
+      q2<=q1;
+    end  
+  end
+
+endmodule
+  
