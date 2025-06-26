821437e2567b149770d5a3633dc40225a67fd8ca
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 01:48:23 +0530

    Update binary2gray.v

--- binary2gray.v
+++ binary2gray.v
@@ -1,1 +1,12 @@
+module binary2gray #(parameter addr_size=4)(
+    input  [addr_size:0] data,
+    output reg [addr_size:0] gray
+);
+    integer i;
+    always @(*) begin
+        gray[addr_size] = data[addr_size];
+        for(i = addr_size; i > 0; i = i - 1)
+            gray[i-1] = data[i] ^ data[i-1];
+    end
+endmodule
 
