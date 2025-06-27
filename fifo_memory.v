28bc5c0d7e40cc43b0ac28d5a2841962dfee662c
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 16:42:20 +0530

    Update fifo_memory.v

--- fifo_memory.v
+++ fifo_memory.v
@@ -1,1 +1,13 @@
+module fifomem #(parameter datawidth =8,parameter addr_width=3;)
+  (input [datawidth-1:0]wdata,input wlcken,wclk,input [addr_width-1:0]wadrr,raddr
+   output reg [addr_width-1:0] rdata);
 
+  localparam fifo_depth=1<<addr_width;
+  
+  // ram having fifodepth no. of location with each location datawidth wide
+  reg [datawidth-1:0]fifo_ram[0:fifo_depth-1];   
+
+  
+
+  
+  
