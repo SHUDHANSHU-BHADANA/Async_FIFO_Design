8f600ae126c74249b2c2555d993c162e500ffbac
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 18:25:09 +0530

    Update fifo_memory.v

--- fifo_memory.v
+++ fifo_memory.v
@@ -1,20 +1,25 @@
-module fifomem #(parameter datawidth =8,parameter addr_width=3)
-  (input [datawidth-1:0]wdata,input wlcken,wclk,input [addr_width-1:0]wadrr,raddr
-   output  [addr_width-1:0] rdata);
+module fifomem #(parameter datawidth = 8, parameter addr_width = 3)
+(
+  input  [datawidth-1:0] wdata,
+  input                  wclken,
+  input                  wclk,
+  input  [addr_width-1:0] waddr,
+  input  [addr_width-1:0] raddr,
+  output [datawidth-1:0] rdata
+);
 
-  localparam fifo_depth=1<<addr_width;
-  
-  // ram having fifodepth no. of location with each location datawidth wide
-  reg [datawidth-1:0]fifo_ram[0:fifo_depth-1];  
+  localparam fifo_depth = 1 << addr_width;
 
-  always@(posedge wclk) begin
-    if(wclken) fifo_ram[waddr]<=wdata;
+  // RAM having 'fifo_depth' locations, each 'datawidth' wide
+  reg [datawidth-1:0] fifo_ram [0:fifo_depth-1];
+
+  // Write logic
+  always @(posedge wclk) begin
+    if (wclken)
+      fifo_ram[waddr] <= wdata;
   end
 
-  assign rdata=fifo_ram[raddr];
+  // Combinational read
+  assign rdata = fifo_ram[raddr];
+
 endmodule
-
-  
-
-  
-  
