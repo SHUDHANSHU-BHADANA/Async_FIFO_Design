c74f188d7a96dc82fc2d6b798e8dd802ee16d9df
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 22:41:38 +0530

    Update top.v

--- top.v
+++ top.v
@@ -47,12 +47,12 @@ module top #(parameter datawidth = 8, addr_width = 3)
   // Read and Write pointer logic
   rptr_and_empty #(.addr_width(addr_width)) s7 (
     .rinc(rinc), .rclk(rclk), .rrst_n(rrst_n),
-    .rq2_ptr(rq2_wptr), .rptr(rptr), .raddr(raddr), .rempty(rempty)
+    .rq2_wptr(rq2_wptr), .rptr(rptr), .raddr(raddr), .rempty(rempty)
   );
 
   wptr_and_full #(.addr_width(addr_width)) s8 (
     .winc(winc), .wclk(wclk), .wrst_n(wrst_n),
-    .wq2_ptr(wq2_rptr), .wptr(wptr), .waddr(waddr), .wfull(wfull)
+    .wq2_rptr(wq2_rptr), .wptr(wptr), .waddr(waddr), .wfull(wfull)
   );
 
   // FIFO memory
