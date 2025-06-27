3966e96947eae2284b36aeb39ddf2d055a35079d
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-06-27 21:59:48 +0530

    Update gray2binary.v

--- gray2binary.v
+++ gray2binary.v
@@ -6,7 +6,7 @@ module gray2binary #(parameter addr_width = 4)(
     integer i;
     always @(*) begin
         binary[addr_width] = gray[addr_width];  // MSB is same
-        for (i = addr_size - 1; i >= 0; i = i - 1)
+        for (i = addr_width - 1; i >= 0; i = i - 1)
             binary[i] = binary[i + 1] ^ gray[i];
     end
 endmodule
