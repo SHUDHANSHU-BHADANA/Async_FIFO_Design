7632a41c4c9f83a35f363f192f65c0a1076c240f
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-08-15 10:38:43 +0530

    Update fifo_memory.v

--- fifo_memory.v
+++ fifo_memory.v
@@ -3,6 +3,7 @@ module fifomem #(parameter datawidth = 8, parameter addr_width = 3)
   input  [datawidth-1:0] wdata,
   input                  wclken,
   input                  wclk,
+  input                  rempty,
   input  [addr_width-1:0] waddr,
   input  [addr_width-1:0] raddr,
   output [datawidth-1:0] rdata
@@ -20,6 +21,6 @@ module fifomem #(parameter datawidth = 8, parameter addr_width = 3)
   end
 
   // Combinational read
-  assign rdata = fifo_ram[raddr];
+  assign rdata = rempty?0:fifo_ram[raddr];
 
 endmodule
